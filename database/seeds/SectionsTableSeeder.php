64b0166d9829dd178352b79127d73c213e453759
Asyraf Ahmad <asyraf@time.com.my>
2022-02-08 22:58:55 +0800

    migate and seeds data for Sections

---
+++ database/seeds/SectionsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Section;
+
+class SectionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->delete();
+
+        $sectionsRecord = [
+            ['id'=>1,'name'=>'Men','status'=>1],
+            ['id'=>2,'name'=>'Women','status'=>1],
+            ['id'=>3,'name'=>'Kids','status'=>1],
+        ];
+
+        // Section:insert($sectionsRecord);
+        DB::table('sections')->insert($sectionsRecord);
+    }
+}
